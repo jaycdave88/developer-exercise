2ddd318b07f3aff1cf8b196d7c9a3a6369365eea
Jay Dave <jaycdave@gmail.com>
2015-05-08 19:03:30 -0700

    final working product + backbone.js app as well

--- javascript/simple_backbone/collections/allQuotes.js
+++ javascript/simple_backbone/collections/allQuotes.js
@@ -1,5 +1,5 @@
-window.Quotes = Backbone.Collection.extend({
+window.Quotes = Backbone.PageableCollection.extend({
   model: Quote,
-  url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
+  url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json',
 });
 
--- javascript/simple_backbone/views/singleQuotesView.js
+++ javascript/simple_backbone/views/singleQuotesView.js
@@ -1,7 +1,7 @@
 window.QuoteEntryView = Backbone.View.extend({
   tagName: "tr",
 
-  template: _.template('<td><%- source %></td> <td><%- quote %></td> <td><%- theme %></td>'),
+  template: _.template('<td><%- source %></td> <td>"<%- quote %>"</td> <td><%- theme %></td>'),
 
   render: function() {
     return this.$el.html(this.template(this.model.attributes));
